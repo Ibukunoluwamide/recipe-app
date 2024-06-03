f2bc7eb3a4c913746a8496acf9dea7c98749fb9c
Alao Olamide Ezekiel <125860905+Ibukunoluwamide@users.noreply.github.com>
2024-06-03 11:52:18 +0100

    search filtering and favourite counts

--- src/components/RecipeCard.vue
+++ src/components/RecipeCard.vue
@@ -1,21 +1,25 @@
 <template>
-  <!-- {{recipes}} -->
+  <!-- {{recipedetailss}} -->
   <section>
-    <div class="grid md:grid-cols-2 lg:grid-cols-4 gap-3 m-3">
+    <div class="">
       <div
         class="max-w-sm bg-white border border-gray-200 rounded-lg shadow"
-        v-for="(recipe, index) in recipes"
-        :key="index"
+       
       >
-        <a href="#">
-          <img class="rounded-t-lg" :src="recipe.image" alt="product image" />
-        </a>
+          <div class="relative">
+            <img class="rounded-t-lg" :src="recipedetails.image" alt="product image" />
+          </div>
+            <div
+              class="float-end inline-flex  w-10 h-10 text-xs font-bold text-white "
+            >
+             <i @click="toggleFav" :class="`${fav?'text-red-500 fa-solid':'fa-regular text-red-500'}  fa-heart text-3xl cursor-pointer`"></i>
+            </div>
         <div class="px-5 pb-5">
-          <a href="#">
+          <RouterLink :to="`/recipes/${recipedetails.id}`">
             <h5 class="text-xl font-semibold tracking-tight text-gray-900">
-              {{ recipe.name }}
+              {{ recipedetails.name }}
             </h5>
-          </a>
+          </RouterLink>
           <div class="flex items-center mt-2.5 mb-5">
             <div class="flex items-center space-x-1 rtl:space-x-reverse">
               <div
@@ -24,7 +28,11 @@
                 :key="index"
               >
                 <svg
-                  class="w-4 h-4 text-yellow-300"
+                  :class="
+                    index+1 <= recipedetails.rating
+                      ? 'w-4 h-4 text-yellow-300'
+                      : 'w-4 h-4 text-gray-300'
+                  "
                   aria-hidden="true"
                   xmlns="http://www.w3.org/2000/svg"
                   fill="currentColor"
@@ -38,40 +46,51 @@
             </div>
             <span
               class="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 d ms-3"
-              >{{ recipe.rating }}</span
+              >{{ recipedetails.rating }}</span
             >
           </div>
           <div class="flex items-center justify-between">
-            <span class="text-xl font-bold text-gray-900"
-              >{{ recipe.prepTimeMinutes }} mins</span
+            <span class="text-xl font-bold text-blue-600"
+              >{{ recipedetails.prepTimeMinutes }} mins</span
             >
             <button
-              class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white  focus:outline-none"
-              data-modal-target="default-modal" data-modal-toggle="default-modal"
+              class="relative inline-flex items-center justify-center p-0.5  overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white focus:outline-none"
+              data-modal-target="default-modal"
+              data-modal-toggle="default-modal"
             >
               <RouterLink
-              :to="`/recipes/${recipe.id}`"
+                :to="`/recipes/${recipedetails.id}`"
                 class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white rounded-md group-hover:bg-opacity-0"
               >
-                Make this
+                View Recipe
               </RouterLink>
             </button>
           </div>
-          
         </div>
       </div>
     </div>
-    
-
-
-
   </section>
 </template>
 
 <script setup>
-defineProps(["recipes"]);
-import { RouterLink } from 'vue-router'
- 
+defineProps(["recipedetails"]);
+import { computed, onMounted, ref } from "vue";
+import { RouterLink } from "vue-router";
+const fav = ref(false)
+const emit = defineEmits('fav')
+const favCount = ref(0)
+// onMounted(()=>{
+//   console.log(recipedetailss);
+// })
+const toggleFav = ()=>{
+  fav.value = !fav.value
+  if (fav.value) {
+    favCount.value++
+  }else{
+    favCount.value--
+  }
+    emit('fav', fav.value)
+}
 </script>
 
 <style>
--- src/views/RecipeHome.vue
+++ src/views/RecipeHome.vue
@@ -1,12 +1,56 @@
 <template>
   <section class="">
-    <div class="">Favourite Count: 0</div>
-    <div class="">
-      <div v-if="recipes">
-        <RecipeCard :recipes="recipes" />
+    <div class="text-2xl font-semibold p-2">Favourite Count: {{favCounter}}</div>
+    <div class="flex justify-between items-center p-3">
+      <div class="">
+        <h1 class="text-3xl font-bold">Recipes</h1>
       </div>
-      <div v-else>
-        <div aria-label="Loading..." role="status" class="flex items-center justify-center mt-4 space-x-2">
+      <div class="">
+        <form class="md:w-[25vw]">
+          <label
+            for="default-search"
+            class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+            >Search</label
+          >
+          <div class="relative">
+            <div
+              class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none"
+            >
+              <svg
+                class="w-4 h-4 text-gray-500 dark:text-gray-400"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 20 20"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                />
+              </svg>
+            </div>
+            <input
+              type="search"
+              id="search"
+              v-model="searchInput"
+              class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
+              placeholder="Search Recipes..."
+              required
+            />
+          </div>
+        </form>
+      </div>
+    </div>
+    <div class="">
+      
+      <div class="grid md:grid-cols-2 lg:grid-cols-4 gap-3 m-3">
+        <RecipeCard  v-for="(recipe,index) in filteredRecipes" :key="index" :recipedetails="recipe" @fav="getFav"/>
+      </div>
+      <div v-if="loading">
+        <div aria-label="Loading..." role="status" class="flex items-center justify-center mt-44 space-x-2">
     <svg class="h-20 w-20 animate-spin stroke-gray-500" viewBox="0 0 256 256">
         <line x1="128" y1="32" x2="128" y2="64" stroke-linecap="round" stroke-linejoin="round" stroke-width="24"></line>
         <line x1="195.9" y1="60.1" x2="173.3" y2="82.7" stroke-linecap="round" stroke-linejoin="round"
@@ -31,17 +75,35 @@
 </template>
 
 <script setup>
-import { onMounted } from "vue";
+import { computed, onMounted, ref } from "vue";
 import useRecipeStore from "@/stores/recipeStore";
 import RecipeCard from "@/components/RecipeCard.vue";
 
 import { storeToRefs } from "pinia";
 const store = useRecipeStore();
 const { recipes } = storeToRefs(store);
+const searchInput = ref('')
+const loading = ref(true)
+const favCounter = ref(0)
 onMounted(() => {
-  store.getAllRecipe();
   // console.log(recipes.value);
+  setTimeout(() => {
+    loading.value = false
+    store.getAllRecipe();
+  }, 3000)
 });
+const filteredRecipes = computed(()=>{
+  return recipes.value.filter((recipe) => recipe.name.toLowerCase().includes(searchInput.value.toLowerCase()))
+})
+const getFav= (e)=>{
+console.log((e));
+if (e) {
+favCounter.value++ 
+}else{
+  favCounter.value-- 
+}
+  console.log(favCounter.value);
+}
 </script>
 
 <style>
